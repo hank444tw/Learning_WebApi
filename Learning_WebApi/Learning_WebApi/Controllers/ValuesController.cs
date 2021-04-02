cb73df4f5333d865be064d83a6b929be4263d140
MSI-GL65-9SCK\hankh <10861113@gm.nfu.edu.tw>
2021-04-02 14:32:04 +0800

    以POSTMAN測試API

--- Learning_WebApi/Learning_WebApi/Controllers/ValuesController.cs
+++ Learning_WebApi/Learning_WebApi/Controllers/ValuesController.cs
@@ -17,6 +17,7 @@ namespace Learning_WebApi.Controllers
         db10861113Entities db = new db10861113Entities();
 
         // GET api/values
+        [AcceptVerbsAttribute("GET","POST")]
         public IHttpActionResult Get()
         {
             var data = db.User;
@@ -24,33 +25,33 @@ namespace Learning_WebApi.Controllers
             return Json(str);
         }
 
-        // GET api/values/5
-        public IHttpActionResult Get(int id)
-        {
-            User data = db.User.Find(id);
-            if(data == null)
-            {
-                return NotFound();
-            }
+        //// GET api/values/5
+        //public IHttpActionResult Get(int id)
+        //{
+        //    User data = db.User.Find(id);
+        //    if(data == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return Json(data);
-        }
+        //    return Json(data);
+        //}
 
-        // POST api/values
-        public void Post([FromBody] string value)
-        {
+        //// POST api/values
+        //public void Post([FromBody] string value)
+        //{
 
-        }
+        //}
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
     }
 
     public class Student
